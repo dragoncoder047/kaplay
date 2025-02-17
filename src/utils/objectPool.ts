008df5c9707ffb0e89ff80a6bda3a8655cf2583b
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2025-02-17 10:57:04 -0500

    use array to avoid allocating objects for linked list nodes
    it defeated the entire purpose of the object pool lol

--- src/utils/objectPool.ts
+++ src/utils/objectPool.ts
@@ -2,31 +2,24 @@ export interface Resettable {
     reset(): void
 }
 
-interface PoolEntry<T extends Resettable> {
-    value: T
-    next?: PoolEntry<T>
-}
-
 export class ObjectPool<T extends Resettable> {
-    unusedObjects: PoolEntry<T> | undefined;
+    unusedObjects: T[] = [];
     factory: () => T;
     constructor(factory: () => T, initialSize: number = 16) {
-        this.unusedObjects = undefined;
+        this.unusedObjects = [];
         this.factory = factory;
         for (var i = 0; i < initialSize; i++) {
             this.release(factory());
         }
     }
     get(): T {
-        if (this.unusedObjects) {
-            const value: T = this.unusedObjects.value;
-            this.unusedObjects = this.unusedObjects.next;
-            return value;
+        if (this.unusedObjects.length > 0) {
+            return this.unusedObjects.pop()!;
         }
         return this.factory();
     }
     release(value: T) {
         value.reset();
-        this.unusedObjects = { value, next: this.unusedObjects };
+        this.unusedObjects.push(value);
     }
 }
