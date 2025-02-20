0252e59a5d60d34f5f58b02ba2ceaef23a421114
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2025-02-19 21:26:27 -0500

    types on SAPboth

--- src/math/spatial/sweepandprune.ts
+++ src/math/spatial/sweepandprune.ts
@@ -85,7 +85,7 @@ export class SweepAndPruneHorizontal {
     /**
      * Iterates all object pairs which potentially collide
      */
-    *[Symbol.iterator]() {
+    *[Symbol.iterator](): Generator<[GameObj<AreaComp>, GameObj<AreaComp>], void, void> {
         const touching = new Set<GameObj<AreaComp>>();
 
         for (const edge of this.edges) {
@@ -185,7 +185,7 @@ export class SweepAndPruneVertical {
     /**
      * Iterates all object pairs which potentially collide
      */
-    *[Symbol.iterator]() {
+    *[Symbol.iterator](): Generator<[GameObj<AreaComp>, GameObj<AreaComp>], void, void> {
         const touching = new Set<GameObj<AreaComp>>();
 
         for (const edge of this.edges) {
@@ -233,15 +233,15 @@ export class SweepAndPruneBoth {
         this.vertical.update();
     }
 
-    *[Symbol.iterator]() {
+    *[Symbol.iterator](): Generator<[GameObj<AreaComp>, GameObj<AreaComp>], void, void> {
         const horizontalColliding = [...this.horizontal];
         const verticalColliding = [...this.vertical];
-        const horizontalMap = new Map<string, GameObj<AreaComp>[]>(horizontalColliding.map(c => [`${c[0].id}-${c[1].id}`, c]));
-        const verticalMap = new Map<string, GameObj<AreaComp>[]>(verticalColliding.map(c => [`${c[0].id}-${c[1].id}`, c]));
+        const horizontalMap = new Map<string, [GameObj<AreaComp>, GameObj<AreaComp>]>(horizontalColliding.map(c => [`${c[0].id}-${c[1].id}`, c]));
+        const verticalMap = new Map<string, [GameObj<AreaComp>, GameObj<AreaComp>]>(verticalColliding.map(c => [`${c[0].id}-${c[1].id}`, c]));
         const intersection = new Set(horizontalMap.keys()).intersection(new Set(verticalMap.keys()));
 
         for (let key in intersection) {
-            yield horizontalMap.get(key);
+            yield horizontalMap.get(key)!;
         }
     }
 }
