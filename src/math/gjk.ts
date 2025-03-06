af9a62f2063346b8422de4c831314b21ce3e64e2
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2025-03-05 20:24:02 -0500

    add check for direction NaNs

--- src/math/gjk.ts
+++ src/math/gjk.ts
@@ -73,6 +73,8 @@ class PolygonCollider implements Collider {
     }
 
     support(direction: Vec2): Vec2 {
+        if (isNaN(direction.x) || isNaN(direction.y))
+            throw new Error("got NaN'ed direction vector");
         let maxPoint;
         let maxDistance = Number.NEGATIVE_INFINITY;
 
